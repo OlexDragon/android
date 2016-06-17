eaa5a674a6f8795ac9bcfdf3d7f684ed1077e48c
OlexDragon <OlexDragon@0dc50752-87c4-43de-a371-da0aadb1ed76>
2016-06-17 00:26:59 +0000

    Test

--- app/src/main/java/com/irttechnologies/gui/android/MainActivity.java
+++ app/src/main/java/com/irttechnologies/gui/android/MainActivity.java
@@ -1,49 +1,28 @@
 package com.irttechnologies.gui.android;
 
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.irttechnologies.gui.R;
-import com.irttechnologies.gui.android.listeners.DrawerItemClickListener;
-
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String PREFS_NAME = "irt_android_gui";
+    public static final String SELECTED_MENU = "selected_menu";
     private Fragment fragment;
+    private DrawerLayout mDrawer;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        String[] menuTitles = getResources().getStringArray(R.array.menu_array);
-        Log.e("menuTitles, ", Arrays.toString(menuTitles));
-
-        // Set the adapter for the list view
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.id.left_drawer, menuTitles);
-        mDrawerList.setAdapter(adapter);
-        // Set the list's click listener
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener(this));
-
-        // However, if we're being restored from a previous state,
-        // then we don't need to do anything and should return or else
-        // we could end up with overlapping fragments.
-        if (savedInstanceState != null) {
-            return;
-        }
-
-        SharedPreferences preferences = getSharedPreferences("androidIrtGui", 0);
-        final int menuId = preferences.getInt("onCreate", R.id.menu_about_us);
-        showFragment(menuId);
+        final SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, 0);
+        showFragment(sharedPreferences.getInt(SELECTED_MENU, R.id.menu_contact_us));
     }
 
     @Override  public boolean onCreateOptionsMenu(Menu menu) {
@@ -54,63 +33,44 @@ public class MainActivity extends AppCompatActivity {
     @Override  public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         int itemId = item.getItemId();
-        SharedPreferences preferences = getSharedPreferences("androidIrtGui", 0);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putInt("selected_menu", itemId);
+
+        final SharedPreferences preferences = getSharedPreferences("androidIrtGui", 0);
+        final SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(SELECTED_MENU, itemId);
+
         return showFragment(itemId);
     }
 
     public boolean showFragment(int menuId) {
         switch (menuId) {
             case R.id.menu_about_us:
-                showAboutUsFragment();
+                showFragment(new AboutUsFragment());
                 return true;
             case R.id.menu_contact_us:
-                showContactUsFragment();
+                showFragment(new ContactUsFragment());
                 return true;
             case R.id.menu_control:
-                showControlFragment();
+                showFragment(new ControlFragment());
                 return true;
             default:
                 return false;//super.onOptionsItemSelected(item);
         }
     }
 
-    private void showControlFragment() {
+    private void showFragment(Fragment fragment) {
         if(findViewById(R.id.main_frame) != null){
             boolean createNew = fragment == null;
 
             // Create a new Fragment to be placed in the activity layout
-            fragment = new ControlFragment();
-            showFragment(createNew);
-        }
-    }
-
-    private void showContactUsFragment() {
-        if(findViewById(R.id.main_frame) != null){
-            boolean createNew = fragment == null;
-
-            // Create a new Fragment to be placed in the activity layout
-            fragment = new ContactUsFragment();
-            showFragment(createNew);
+             showFragment(createNew, fragment);
 
 
         }
     }
 
-    private void showAboutUsFragment() {
-        if(findViewById(R.id.main_frame) != null){
-            boolean createNew = fragment == null;
+    private void showFragment(boolean createNew, Fragment fragment) {
+        this.fragment = fragment;
 
-            // Create a new Fragment to be placed in the activity layout
-            fragment = new AboutUsFragment();
-            showFragment(createNew);
-
-
-        }
-    }
-
-    private void showFragment(boolean createNew) {
         // Add the fragment to the 'fragment_container' FrameLayout
         FragmentTransaction transaction = getSupportFragmentManager()
                                                         .beginTransaction();
